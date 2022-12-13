3eb23eaf3997b32ab01375fe88339c02f15eae43
sham843 <sham.jadhav@shauryatechnosoft.com>
2022-12-13 13:54:03 +0530

    routing changs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), data: { breadcrumb: [{ title: 'Home', active: true }] } }, 
+const routes: Routes = [
+{ path: '', redirectTo: 'home', pathMatch: 'full' },
+{ path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), data: { breadcrumb: [{ title: 'Home', active: true }] } }, 
 { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule), data: { breadcrumb: [{ title: 'Login', active: true }] } },
 { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard], data: { breadcrumb: [{ title: 'Dashboard', active: true }]}},
 { path: 'designation-master', loadChildren: () => import('./modules/masters/designation-master/designation-master.module').then(m => m.DesignationMasterModule), canActivate: [AuthGuard], data: { breadcrumb: [{ title: 'Designation Master', active: true }]}},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { SchoolRegistrationComponent } from './modules/masters/school-registration/school-registration.component';
-import { TeacherRegistrationComponent } from './modules/masters/teacher-registration/teacher-registration.component';
-import { StudentRegistrationComponent } from './modules/masters/student-registration/student-registration.component';
-import { OfficeUsersComponent } from './modules/masters/office-users/office-users.component';
-import { AgencyRegistrationComponent } from './modules/masters/agency-registration/agency-registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SchoolRegistrationComponent,
-    TeacherRegistrationComponent,
-    StudentRegistrationComponent,
-    OfficeUsersComponent,
-    AgencyRegistrationComponent
   ],
   imports: [
     BrowserModule,
