14c3e5acfc09bea9b41830e754ccf574346d1c97
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-12-14 13:39:46 +0530

    added layout of the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { filter } from 'rxjs';
 })
 export class AppComponent {
   title = 'zilla Parishad Osmanabad';
-  isLoggedIn: boolean = false;
+  isLoggedIn: boolean = true;
 
   constructor(private router: Router, private titleService: Title, private activatedRoute: ActivatedRoute) {
     this.checkBaseUrl();
