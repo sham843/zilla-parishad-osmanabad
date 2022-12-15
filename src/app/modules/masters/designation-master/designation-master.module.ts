68b469305a7405eedf59bed3108f9591c2840a81
rahulmistari2141 <rahulmistari2141@gmail.com>
2022-12-15 10:03:58 +0530

    designation-master page design

--- src/app/modules/masters/designation-master/designation-master.module.ts
+++ src/app/modules/masters/designation-master/designation-master.module.ts
@@ -6,6 +6,13 @@ import { DesignationMasterComponent } from './designation-master.component';
 import { AddUpdateDesignationMasterComponent } from './add-update-designation-master/add-update-designation-master.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { SharedModule } from 'src/app/shared/shared.module';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +24,13 @@ import { SharedModule } from 'src/app/shared/shared.module';
     DesignationMasterRoutingModule,
     MatDialogModule,
     SharedModule,
+    MatSelectModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatMenuModule,
+    MatButtonModule
+    
   ]
 })
 export class DesignationMasterModule { }
