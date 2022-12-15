ce05bdc3f03aba49a2ecd7edfc44e5fac71f4ab3
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-12-15 15:46:48 +0530

    dashboard page redesigned

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,7 +5,7 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { AgmCoreModule } from '@agm/core';
 import {MatSelectModule} from '@angular/material/select';
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -15,6 +15,7 @@ import {MatSelectModule} from '@angular/material/select';
     CommonModule,
     DashboardRoutingModule,
     MatSelectModule,
+    MatCardModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBhkYI4LMEqVhB6ejq12wpIA6CW5theKJw',
       language: 'en',
