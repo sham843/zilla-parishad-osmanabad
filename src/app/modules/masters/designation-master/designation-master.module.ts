a824def35013bdec59c8976cef07e6f5b16b5a0f
rahulmistari2141 <rahulmistari2141@gmail.com>
2022-12-15 10:09:46 +0530

    import material modal

--- src/app/modules/masters/designation-master/designation-master.module.ts
+++ src/app/modules/masters/designation-master/designation-master.module.ts
@@ -6,7 +6,7 @@ import { DesignationMasterComponent } from './designation-master.component';
 import { AddUpdateDesignationMasterComponent } from './add-update-designation-master/add-update-designation-master.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { SharedModule } from 'src/app/shared/shared.module';
-<<<<<<< HEAD
+
 import {MatSelectModule} from '@angular/material/select';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -14,9 +14,8 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 
-=======
 import { DesignationMasterService } from './designation-master.service';
->>>>>>> 3050612ed9f4c2dc16931e4e521fb28ee50e5257
+
 
 @NgModule({
   declarations: [
@@ -28,7 +27,6 @@ import { DesignationMasterService } from './designation-master.service';
     DesignationMasterRoutingModule,
     MatDialogModule,
     SharedModule,
-<<<<<<< HEAD
     MatSelectModule,
     MatCardModule,
     MatFormFieldModule,
@@ -36,8 +34,7 @@ import { DesignationMasterService } from './designation-master.service';
     MatMenuModule,
     MatButtonModule
     
-  ]
-=======
+
   ],
   providers:[DesignationMasterService]
 >>>>>>> 3050612ed9f4c2dc16931e4e521fb28ee50e5257
