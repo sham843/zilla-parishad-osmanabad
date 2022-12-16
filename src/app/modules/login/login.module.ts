2c554931e3a262b111532773d331463d87b8ca8c
navinUI-stpl <navin.boddhul@shauryatechnosoft.com>
2022-12-16 15:33:56 +0530

    added buttons

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatFormFieldModule,
+    MatTabsModule
   ]
 })
 export class LoginModule { }
