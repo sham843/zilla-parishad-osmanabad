96ca5b8fefe81ed6d5e01096a4e0440a298d159b
rahulmistari2141 <rahulmistari2141@gmail.com>
2022-12-16 19:09:50 +0530

    forgot-password page design and zp logo add in assets folder

--- src/app/modules/forgot-password/forgot-password.module.ts
+++ src/app/modules/forgot-password/forgot-password.module.ts
@@ -3,6 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
 import { ForgotPasswordComponent } from './forgot-password.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -11,7 +22,18 @@ import { ForgotPasswordComponent } from './forgot-password.component';
   ],
   imports: [
     CommonModule,
-    ForgotPasswordRoutingModule
+    ForgotPasswordRoutingModule,
+    SharedModule,
+    MatSelectModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule
   ]
 })
 export class ForgotPasswordModule { }
