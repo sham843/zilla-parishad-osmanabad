bf800882098ce78cc7fb35118d3e84864aad0cf9
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-12-19 14:06:27 +0530

    layout changed

--- src/app/layouts/secure/secure.component.ts
+++ src/app/layouts/secure/secure.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
 
 @Component({
   selector: 'app-secure',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./secure.component.scss']
 })
 export class SecureComponent {
+  classname: any;
 
+  constructor(private webStorage: WebStorageService) {
+  }
+
+  ngOnInit() {
+    this.webStorage.getTheme().subscribe((res: any) => {
+      this.classname = res;
+    })
+  }
 }
