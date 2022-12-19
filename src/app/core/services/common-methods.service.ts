39caa204a857752bb26d7dee6914a5c32b731876
deepalighume <deepali.ghume@shauryatechnosoft.com>
2022-12-19 15:35:54 +0530

    added cardgrid detail

--- src/app/core/services/common-methods.service.ts
+++ src/app/core/services/common-methods.service.ts
@@ -8,8 +8,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class CommonMethodsService {
 
   constructor(private SnackBar: MatSnackBar,
-              private router: Router,
-              private activatedRoute: ActivatedRoute) { }
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
 
   snackBar(data: string, status: number) {
     let snackClassArr: any = ['snack-success', 'snack-danger', 'snack-warning'];
@@ -25,7 +25,7 @@ export class CommonMethodsService {
     this.SnackBar.dismiss();
   }
 
-  checkEmptyData(data: any){
+  checkEmptyData(data: any) {
     let value: any;
     if (data == "" || data == null || data == "null" || data == undefined || data == "undefined" || data == 'string' || data == null || data == 0) {
       value = false;
@@ -39,11 +39,11 @@ export class CommonMethodsService {
     this.router.navigate([path], { relativeTo: this.activatedRoute })
   }
 
-  isEmptyArray(array: any){
-    if (array == null || array == undefined || array.length <= 0){
-        return true;
-    }else{
-        return false;
+  isEmptyArray(array: any) {
+    if (array == null || array == undefined || array.length <= 0) {
+      return true;
+    } else {
+      return false;
     }
   }
 
@@ -56,49 +56,47 @@ export class CommonMethodsService {
     a.href = url;
     a.download = filename;
     a.click();
-}
+  }
 
 
-downLoadPdf(header: any, values: any, objData: any) {
-  // let doc: any = new jsPDF();
-  let doc: any;
+  downLoadPdf(header: any, values: any, objData: any) {
+    // let doc: any = new jsPDF();
+    let doc: any;
 
-  doc.autoTable(header, values, {
-    startY: 25,
-    // margin: { horizontal: 7 , verticle: 10},
-    margin: { top: 25 },
+    doc.autoTable(header, values, {
+      startY: 25,
+      // margin: { horizontal: 7 , verticle: 10},
+      margin: { top: 25 },
 
-    didDrawPage: function (_data: any) {
-      var imgWidth = 33;
-      var height = 20;
-      doc.addImage('../../../../assets/images/.jpeg', 'JPEG', 2, -3, imgWidth, height); //add image
+      didDrawPage: function (_data: any) {
+        var imgWidth = 33;
+        var height = 20;
+        doc.addImage('../../../../assets/images/.jpeg', 'JPEG', 2, -3, imgWidth, height); //add image
 
-      doc.setFontSize(13);
-      doc.text(objData.topHedingName, 100, 8, "center");
+        doc.setFontSize(13);
+        doc.text(objData.topHedingName, 100, 8, "center");
 
-      if(objData?.timePeriod != null){
-      doc.setFontSize(8);
-      doc.text(objData.timePeriod, 11, 14, "left");
+        if (objData?.timePeriod != null) {
+          doc.setFontSize(8);
+          doc.text(objData.timePeriod, 11, 14, "left");
+        }
+
+        doc.setFontSize(8);
+        doc.text(objData.createdDate, 200, 14, "right");
+
+        doc.setLineWidth(0.2);
+        doc.line(12, 15, 200, 15);
+
+        doc.setLineWidth(0.2);
+        doc.line(12, 286, 200, 286);
+
+        doc.setFontSize(8);
+        doc.text('Note:This is a system generated File.', 200, 290, "right");
+
       }
-     
-       doc.setFontSize(8);
-       doc.text(objData.createdDate, 200, 14, "right");
+    });
 
-      doc.setLineWidth(0.2);
-      doc.line(12, 15, 200, 15);
-
-      doc.setLineWidth(0.2);
-      doc.line(12, 286, 200, 286);
-
-      doc.setFontSize(8);
-      doc.text('Note:This is a system generated File.', 200, 290, "right");
-
-    }
-  });
-
-  doc.save(objData.topHedingName);
-}
-
-
+    doc.save(objData.topHedingName);
+  }
 
 }
