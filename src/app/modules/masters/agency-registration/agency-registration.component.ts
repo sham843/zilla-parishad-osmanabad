a395f995c1590c948654611e55a9e256a2759535
OmkarDivekar-ShauryaTech <omkar.divekar@shauryatechnosoft.com>
2022-12-19 12:12:42 +0530

    table data binded to agency reg page

--- src/app/modules/masters/agency-registration/agency-registration.component.ts
+++ src/app/modules/masters/agency-registration/agency-registration.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ApiService } from 'src/app/core/services/api.service';
+import { ErrorsService } from 'src/app/core/services/errors.service';
 import { AddUpdateAgencyRegistrationComponent } from './add-update-agency-registration/add-update-agency-registration.component';
 
 @Component({
@@ -8,7 +10,48 @@ import { AddUpdateAgencyRegistrationComponent } from './add-update-agency-regist
   styleUrls: ['./agency-registration.component.scss']
 })
 export class AgencyRegistrationComponent {
-  constructor(private dialog: MatDialog) { }
+  pageNumber: number = 1;
+  constructor(private dialog: MatDialog, private apiService : ApiService, private errors : ErrorsService ) { }
+
+  ngOnInit() {
+    this.getTableData()
+  }
+
+  getTableData(flag?: string) {
+    this.pageNumber = flag == 'filter' ? 1 : this.pageNumber;
+    let tableDataArray = new Array();
+    let tableDatasize!: Number;
+    let str = `pageno=${this.pageNumber}&pagesize=10`;
+    this.apiService.setHttp('GET', 'zp-osmanabad/Agency/GetAll?' + str, false, false, false, 'baseUrl');
+    this.apiService.getHttp().subscribe({
+
+      next: (res: any) => {
+        if (res.statusCode == "200") {
+          tableDataArray = res.responseData.responseData1;
+          tableDatasize = res.responseData.responseData2.pageCount;
+        } else {
+          tableDataArray = [];
+          tableDatasize = 0;
+        }
+        let displayedColumns = ['srNo', 'agency_Name', 'contact_No', 'emailId', 'action'];
+        let displayedheaders = ['Sr.No', 'Name', 'Contact No.', 'Email ID', 'Action'];
+        let tableData = {
+          pageNumber: this.pageNumber,
+          img: '', blink: '', badge: '', isBlock: '', pagintion: true,
+          displayedColumns: displayedColumns, tableData: tableDataArray,
+          tableSize: tableDatasize,
+          tableHeaders: displayedheaders
+        };
+        this.apiService.tableData.next(tableData);
+      },
+      error: ((err: any) => { this.errors.handelError(err) })
+    });
+  }
+
+  onPagintion(pageNo: number) {
+    this.pageNumber = pageNo;
+    this.getTableData()
+  }
 
   childCompInfo(_obj: any) {
     // switch (obj.label) {
