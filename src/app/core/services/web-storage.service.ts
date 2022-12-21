8b87afc0eac5a5a989d58889a98ae02ae0801eac
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2022-12-21 14:51:57 +0530

    global_file_changes

--- src/app/core/services/web-storage.service.ts
+++ src/app/core/services/web-storage.service.ts
@@ -26,4 +26,33 @@ export class WebStorageService {
   getLangauge(){
     return this.language.asObservable();
   }
+
+  checkUserIsLoggedIn() { // check user isLoggedIn or not
+    let sessionData: any = sessionStorage.getItem('loggedIn');
+    sessionData == null || sessionData == '' ? localStorage.clear() : '';
+    if (localStorage.getItem('loggedInData') && sessionData == 'true') return true;
+    else return false;
+  }
+
+  getLoggedInLocalstorageData() {
+    if (this.checkUserIsLoggedIn() == true) {
+      let data = JSON.parse(localStorage['loggedInData']);
+      return data;
+    }
+  }
+
+  getUserId(){
+    let data =this.getLoggedInLocalstorageData();
+    return data.responseData.id
+  }
+
+  createdByProps(): any {
+    return {
+      "createdBy": this.getUserId() || 0,
+      "modifiedBy": this.getUserId() || 0,
+      "createdDate": new Date(),
+      "modifiedDate": new Date(),
+      "isDeleted": true
+    }
+  }
 }
