81e9ed750cb4a94597479e5fd2120ebb09f87744
OmkarDivekar-ShauryaTech <omkar.divekar@shauryatechnosoft.com>
2022-12-22 12:32:37 +0530

    change in forgot pass

--- src/app/modules/forgot-password/forgot-password.component.ts
+++ src/app/modules/forgot-password/forgot-password.component.ts
@@ -5,6 +5,7 @@ import { ApiService } from 'src/app/core/services/api.service';
 import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
 import { ErrorsService } from 'src/app/core/services/errors.service';
 import { ValidationService } from 'src/app/core/services/validation.service';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -25,12 +26,13 @@ export class ForgotPasswordComponent {
   timeLeft: number = 60;
   interval: any;
   otpStatus: boolean = false;
+  webData = this.webStorage.createdByProps();
   obj = {
-    "createdBy": 0,
-    "modifiedBy": 0,
-    "createdDate": new Date(),
-    "modifiedDate": new Date(),
-    "isDeleted": true,
+    "createdBy": this.webData.createdBy,
+    "modifiedBy": this.webData.modifiedBy,
+    "createdDate": this.webData.createdDate,
+    "modifiedDate": this.webData.modifiedDate,
+    "isDeleted": this.webData.isDeleted,
     "id": 0,
     "mobileNo": "",
     "otp": "",
@@ -41,7 +43,7 @@ export class ForgotPasswordComponent {
   }
 
   constructor(private apiService: ApiService, private common: CommonMethodsService, private router: Router,
-    private errors: ErrorsService, public validation: ValidationService, private fb: FormBuilder) { }
+    private errors: ErrorsService, public validation: ValidationService, private fb: FormBuilder, public webStorage : WebStorageService) { }
 
 
   ngOnInit() {
@@ -85,6 +87,7 @@ export class ForgotPasswordComponent {
 
   sendOtp(flag: any) {
     this.sendOtpFlag = true;
+   
     this.mobileForm.invalid ? this.common.snackBar('Please Enter Mobile Number', 1) : '';
     let obj = this.mobileForm.value;
     this.obj.mobileNo = obj.mobileNo;
@@ -95,7 +98,7 @@ export class ForgotPasswordComponent {
       this.apiService.setHttp('post', 'api/OtpTran', false, this.obj, false, 'baseUrl');
       this.apiService.getHttp().subscribe({
         next: (res: any) => {
-          res.statusCode == 200 ? (this.common.snackBar(res.statusMessage, 0),this.setFlag(), this.startTimer()) : this.common.snackBar(res.statusMessage, 1);
+          res.statusCode == 200 ? (this.common.snackBar(res.statusMessage, 0),this.setFlag(flag), this.startTimer()) : this.common.snackBar(res.statusMessage, 1);
         },
         error: ((err: any) => { this.errors.handelError(err) })
       })
@@ -104,11 +107,12 @@ export class ForgotPasswordComponent {
 
   pauseTimer() {
     clearInterval(this.interval);
-    this.mobileForm.value.mobileNo.setValue('');
+  //  this.fcMobile['mobileNo'].setValue('');
   }
 
-  setFlag(){
-   this.sendOtpFlag ?(this.mobileField = false, this.otpField = true, this.otpStatus = true) :'';
+  setFlag(flag? :any){
+    // flag == 'resend' ? (this.mobileField=false, this.otpField= true, this.otpStatus = true ) : ''
+   this.sendOtpFlag || flag == 'resend' ?(this.mobileField = false, this.otpField = true, this.otpStatus = true) :'';
    this.verifyOtpflag ? (this.mobileField = false, this.otpField = false, this.passwordField = true):'';
   }
 
