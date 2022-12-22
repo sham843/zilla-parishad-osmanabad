3fe591c7a00ce1a1d4f6ff1d78d176ce86bfa91c
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2022-12-22 19:02:50 +0530

    authguard_and_logout_functionality_implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
+import { CheckLoggedInGuard } from './core/guards/check-logged-in.guard';
 import { PublicComponent } from './layouts/public/public.component';
 import { SecureComponent } from './layouts/secure/secure.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '', component: PublicComponent,loadChildren: () => import('./layouts/public/public.module').then(m => m.PublicModule) },
+  { path: '', canActivate: [CheckLoggedInGuard], component: PublicComponent,loadChildren: () => import('./layouts/public/public.module').then(m => m.PublicModule) },
   {
     path: '',
     canActivate: [AuthGuard],
