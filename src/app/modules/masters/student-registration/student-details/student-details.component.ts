2baead4f4edd4c84c58dce52ca9359bfcde7d427
prakharPande-stpl <prakhar.pande@shauryatechnosoft.com>
2022-12-23 16:51:19 +0530

    changes in student registration

--- src/app/modules/masters/student-registration/student-details/student-details.component.ts
+++ src/app/modules/masters/student-registration/student-details/student-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-student-details',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-details.component.scss']
 })
 export class StudentDetailsComponent {
+  
+
+  constructor(public dialogRef: MatDialogRef<StudentDetailsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { 
+      console.log(this.data);
+      
+    }
 
 }
