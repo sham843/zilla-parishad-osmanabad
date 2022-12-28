4a29195019fff33877fa1bba11516a7f710cbee2
OmkarDivekar-ShauryaTech <omkar.divekar@shauryatechnosoft.com>
2022-12-28 17:19:56 +0530

    change in my profile

--- src/app/shared/components/my-profile/my-profile.component.ts
+++ src/app/shared/components/my-profile/my-profile.component.ts
@@ -4,6 +4,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { ApiService } from 'src/app/core/services/api.service';
+import { ErrorsService } from 'src/app/core/services/errors.service';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
 
 
 @Component({
@@ -20,5 +23,24 @@ import {MatInputModule} from '@angular/material/input';
   ]
 })
 export class MyProfileComponent {
+userDetails = new Array();
+  constructor(private api : ApiService , private error : ErrorsService , private webStorage : WebStorageService)  {}
+
+  ngOnInit(){
+    this.getUserById();
+  }
+
+  getUserById(){
+    this.api.setHttp('get', `zp_osmanabad/app-login/GetTeacherProfile?TeacherId=${this.webStorage.getUserId()}` , false, false, false, 'baseUrl');
+        this.api.getHttp().subscribe({
+          next: (res: any) => {
+            console.log(res);
+            res.statusCode == 200 ? this.userDetails = res.responseData : this.userDetails = []; 
+          },
+          error: (error: any) => {
+            this.error.handelError(error.statusMessage)
+          }
+  })
+}
 
 }
