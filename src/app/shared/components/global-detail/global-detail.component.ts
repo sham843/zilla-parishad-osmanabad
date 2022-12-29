810694d3c5901fa45dc246e0ff1c5f9a8981341d
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2022-12-29 17:53:16 +0530

    global_details_modal_close_functionality

--- src/app/shared/components/global-detail/global-detail.component.ts
+++ src/app/shared/components/global-detail/global-detail.component.ts
@@ -1,20 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TranslateModule } from '@ngx-translate/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
-
-
-interface Food {
-  value: string;
-  viewValue: string;
-}
-
 @Component({
   selector: 'app-global-detail',
   templateUrl: './global-detail.component.html',
@@ -31,13 +23,11 @@ interface Food {
   ]
 })
 export class GlobalDetailComponent {
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
-  ];
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any){
+  
+  constructor(public dialogRef: MatDialogRef<GlobalDetailComponent>,@Inject(MAT_DIALOG_DATA) public data: any){  }
 
+  closeDialog(){
+    this.dialogRef.close();
   }
 
 }
