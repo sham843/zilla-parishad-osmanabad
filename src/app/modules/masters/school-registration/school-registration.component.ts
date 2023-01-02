eda655c1af536a3955835993ba86a54e4e7b4e74
OmkarDivekar-ShauryaTech <omkar.divekar@shauryatechnosoft.com>
2023-01-02 13:09:24 +0530

    changes in school reg

--- src/app/modules/masters/school-registration/school-registration.component.ts
+++ src/app/modules/masters/school-registration/school-registration.component.ts
@@ -38,7 +38,7 @@ export class SchoolRegistrationComponent {
   displayedColumns = new Array();
   tableDatasize!: Number;
   tableData: any;
-  displayedheadersEnglish = ['#', 'Sr. No.', 'Name', 'Village', 'Taluka', 'District', 'Action'];
+  displayedheadersEnglish = ['#', 'Sr. No.', 'School Name', 'Village', 'Taluka', 'District', 'Action'];
   displayedheadersMarathi = ['#', 'अनुक्रमांक', 'शाळेचे नाव', 'गाव', 'तालुका', 'जिल्हा', 'कृती'];
 
   constructor(private dialog: MatDialog, private apiService: ApiService, private errors: ErrorsService,
