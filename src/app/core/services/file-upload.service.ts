fa99db68febcfa08835ea36a18ebddb96b17c43a
deepalighume <deepali.ghume@shauryatechnosoft.com>
2023-01-03 12:02:41 +0530

    added multiple uploads fn

--- src/app/core/services/file-upload.service.ts
+++ src/app/core/services/file-upload.service.ts
@@ -65,5 +65,64 @@ export class FileUploadService {
 
 
 
+  uploadMultipleDocument(event: any, _folderName?: any, allowedDocTypes?: any) {
+    this.spinner.show();
+    let docTypeCheckFlag = true;
+    return new Observable(obj => {
+      const formData = new FormData();
+      const selResult1 = event.target.value.split('.');
+      const docExt1 = selResult1.pop().toLowerCase();
+      let size = docExt1 == 'pdf' ? 500485760 : 10485760;
+
+      if (event.target.files && event.target.files[0]) {
+        var filesAmount = event.target.files.length;
+        for (let i = 0; i < filesAmount; i++) {
+          if (event.target.files[i].size > size) {
+            obj.error("Required file size should be less than " + (docExt1 == 'pdf'? 500 : 10 )+ " MB.");
+            this.commonService.showPopup("Required file size should be less than " +(docExt1 == 'pdf'? 500 : 10 ) + " MB.", 1);
+            docTypeCheckFlag = false;
+          } else {
+            formData.append("files", event.target.files[i]);
+            let nameText = event.target.files[i].name;
+            const selResult = nameText.split('.');
+            const docExt = selResult.pop();
+            const docExtLowerCase = docExt.toLowerCase();
+            if (allowedDocTypes.match(docExtLowerCase)) { }
+            else {
+              docTypeCheckFlag = false;
+            }
+          }
+        }
+      }
+
+      if (docTypeCheckFlag == true) {
+        this.apiService.setHttp('post', 'whizhack_cms/upload/upload-multiple-photos', false, formData, false, 'whizhackService');
+        this.apiService.getHttp().subscribe({
+          next: (res: any) => {
+            if (res.statusCode === "200") {
+              this.spinner.hide();
+              obj.next(res);
+            }
+            else {
+              this.spinner.hide();
+              this.commonService.checkEmptyData(res.statusMessage) == false ? this.error.handelError(res.statusCode) : ''// this.toastrService.error(res.statusMessage);
+            }
+          },
+          error: ((error: any) => {
+            this.spinner.hide();
+            this.error.handelError(error.status);
+          })
+        })
+      }
+      else {
+        this.spinner.hide();
+        obj.error("Only " + allowedDocTypes + " file format allowed.");
+        this.commonService.showPopup("Only " + allowedDocTypes + " file format allowed.", 1);
+      }
+    })
+  }
+
+
+
 
 }
