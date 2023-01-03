9c170a6b60724e3e26db8c70eb7bedd83cb7622d
stpl-mahesh <100754388+stpl-mahesh@users.noreply.github.com>
2023-01-03 18:51:18 +0530

    changes in dashboard

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -440,6 +440,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
       console.log(index)
     }
     
+    
     this.getSubject(obj.groupId);
     this.getBarChart(obj);
     setTimeout(() => {
