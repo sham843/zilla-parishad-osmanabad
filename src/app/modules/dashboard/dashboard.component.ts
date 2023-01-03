2abdd3db860b029c96992b956d53224e0910f673
stpl-mahesh <100754388+stpl-mahesh@users.noreply.github.com>
2023-01-03 10:55:56 +0530

    changes in dashboard for api change  for school

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -52,7 +52,7 @@ export class DashboardComponent implements OnInit,AfterViewInit {
   
   ngOnInit() {
     this.filterForm = this.fb.group({
-      talukaId: [],
+      talukaId: [4],
       centerId: [],
       schoolId: []
     })
@@ -76,6 +76,7 @@ export class DashboardComponent implements OnInit,AfterViewInit {
       this.talukaData.push({ "id": 0, "taluka": "All", "m_Taluka": "सर्व"}, ...res.responseData);
       const obj=this.talukaData.find((x:any)=> x.taluka=="Osmanabad")
       this.f['talukaId'].patchValue(obj.id);
+      this.getCenters() ;
     })
   }
   getCenters() {
@@ -87,7 +88,7 @@ export class DashboardComponent implements OnInit,AfterViewInit {
   }
   getschools() {
     this.fBgraph['filtercenterId'].patchValue(this.f['centerId'].value)
-    this.masterService.getAllSchoolsByCenterId('', (this.f['centerId'].value|0)).subscribe((res: any) => {
+    this.masterService.getAllSchoolByCriteria('',(this.f['talukaId'].value|0),0, (this.f['centerId'].value|0)).subscribe((res: any) => {
       this.schoolData = res.responseData;
     })
   }
@@ -247,7 +248,16 @@ export class DashboardComponent implements OnInit,AfterViewInit {
         // opacity: 1
       },
       legend: {
-        position: "bottom",
+        position: 'right',
+        fontSize: '10px',
+        show: true,
+        markers: {
+          width: 12,
+          height: 12,
+          strokeWidth: 0,
+          strokeColor: '#fff',
+          fillColors: ['#CB4B4B', '#E76A63', '#E98754', '#EFB45B', '#65C889', '#73AFFE'],
+        }
       }
     };
 
@@ -474,6 +484,7 @@ export class DashboardComponent implements OnInit,AfterViewInit {
           })
           this.barchartOptions1.series.push(arrayObjectData)
           this.barchartOptions1.xaxis.categories.push(...talukaSet);
+          console.log(this.barchartOptions1)
           this.showBarChartS=true;
         }
 
@@ -495,16 +506,11 @@ export class DashboardComponent implements OnInit,AfterViewInit {
         if (res.statusCode == "200") {
           this.tableDataTopPerformance.push(res.responseData.responseData1);
           this.tableDataTopPerformance.push(res.responseData.responseData2);
-          
           this.displayedheaders = ['#','Sr. No.', 'Name', 'Total Student', 'Percetage'];
-
         }
         else{
            this.tableDataTopPerformance = [];
         }
-          
-        
-
       },
       error: (error:any) => { this.error.handelError(error.message) }
     });
