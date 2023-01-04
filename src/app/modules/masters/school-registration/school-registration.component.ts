fbb88b9f05a1b1de4f49d2164beda51505259eb3
Shilpa-ShauryaTechnosoft <shilpa.ahire@shauryatechnosoft.com>
2023-01-04 17:48:43 +0530

    Display Profile Image in gobal details and Table

--- src/app/modules/masters/school-registration/school-registration.component.ts
+++ src/app/modules/masters/school-registration/school-registration.component.ts
@@ -262,6 +262,7 @@ export class SchoolRegistrationComponent {
     var data = {
       headerImage: obj.uploadImage,
       header: this.webStorageS.languageFlag == 'EN' ? obj.schoolName : obj.m_SchoolName,
+      subheader : this.webStorageS.languageFlag == 'EN' ? obj.village : obj.m_Village,
       labelHeader: this.webStorageS.languageFlag == 'EN' ? ['Kendra Name', 'Taluka','District','Lowest Class', 'HighestClass', 'Total Students'] : ['केंद्राचे नाव', 'तालुका', 'जिल्हा', 'सर्वात खालचा वर्ग', 'सर्वोच्च वर्ग', 'एकूण विद्यार्थी'],
       labelKey: this.webStorageS.languageFlag == 'EN' ? ['center','taluka', 'district','lowestClass','highestClass','studentCount'] : ['m_Center', 'm_Taluka','m_District', 'lowestClass', 'highestClass','studentCount'],
       Obj: obj,
