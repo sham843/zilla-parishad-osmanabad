ce5c1b4ede4a2ff7c558cd0b4bdfc4656301396a
OmkarDivekar-ShauryaTech <omkar.divekar@shauryatechnosoft.com>
2023-01-09 11:33:47 +0530

    changes in agency reg

--- src/app/modules/masters/agency-registration/agency-registration.component.ts
+++ src/app/modules/masters/agency-registration/agency-registration.component.ts
@@ -99,7 +99,7 @@ export class AgencyRegistrationComponent {
       this.agencyReport.push(obj);
     });
     if (this.agencyReport.length) {
-      let keyPDFHeader = ['SrNo', "Name", "Contact No.", "Email Id"];
+      let keyPDFHeader = ['SrNo', "Agency Name", "Agency Mobile No.", "Agency Email ID"];
       let ValueData =
         this.agencyReport.reduce(
           (acc: any, obj: any) => [...acc, Object.values(obj).map((value) => value)], []
