9fb74d19af3a7299a85ac34aed07859fdf16c2ef
prakharPande-stpl <prakhar.pande@shauryatechnosoft.com>
2023-01-09 15:32:37 +0530

    chart view for  student reg page

--- src/app/shared/components/global-detail/global-detail.component.ts
+++ src/app/shared/components/global-detail/global-detail.component.ts
@@ -3,18 +3,19 @@ import { Component, Inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TranslateModule } from '@ngx-translate/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { WebStorageService } from 'src/app/core/services/web-storage.service';
 import { LightboxModule } from '@ngx-gallery/lightbox';
-import { Gallery,ImageItem,GalleryModule,} from '@ngx-gallery/core';
+import { Gallery, ImageItem, GalleryModule, } from '@ngx-gallery/core';
+import { progressChartLineComponent } from '../../progressChartLine/progressChartLine.component';
 @Component({
   selector: 'app-global-detail',
   templateUrl: './global-detail.component.html',
   styleUrls: ['./global-detail.component.scss'],
-  standalone : true,
+  standalone: true,
   imports: [
     TranslateModule,
     MatCardModule,
@@ -23,62 +24,49 @@ import { Gallery,ImageItem,GalleryModule,} from '@ngx-gallery/core';
     MatSelectModule,
     MatIconModule,
     MatButtonModule,
-     LightboxModule,
-     GalleryModule
+    LightboxModule,
+    GalleryModule,
+    progressChartLineComponent,
+
   ]
 })
 export class GlobalDetailComponent {
-  items: any=[];
+  items: any = [];
   dataArray = new Array();
-  // [
-    // {
-    //   docPath : '',
-    //   // documentId : 3,
-    //   // id : 283,
-    //   // schoolId : 1863
-    // }
-    // {
-      // docPath : '',
-      // srcUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg',
-      // previewUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg'
-    // },
-    // {
-    //   srcUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg',
-    //   previewUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg'
-    // },
-    // {
-    //   srcUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg',
-    //   previewUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg'
-    // },
-    // {
-    //   srcUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg',
-    //   previewUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg'
-    // }
-  // ]
 
 
-  constructor( public gallery: Gallery,public dialogRef: MatDialogRef<GlobalDetailComponent>,@Inject(MAT_DIALOG_DATA) public data: any, public webStorage: WebStorageService){ 
+  constructor(private webService: WebStorageService,
+    public gallery: Gallery, public dialogRef: MatDialogRef<GlobalDetailComponent>, @Inject(MAT_DIALOG_DATA) public data: any, public webStorage: WebStorageService) {
 
-   }
+  }
 
-   ngOnInit() {
-    console.log("Obj : ",this.data);
-
+  ngOnInit() {
     this.dataArray = this.data.Obj.schoolDocument;
-    
+
     this.items = this.dataArray?.map(item =>
       new ImageItem({ src: item.docPath, thumb: item.docPath })
     );
-    
-    this.basicLightboxExample();
-   }
 
-   basicLightboxExample() {
+    this.basicLightboxExample();
+    this.getLineChartDetails(this.data);
+  }
+
+  basicLightboxExample() {
     this.gallery.ref().load(this.items);
   }
 
-  closeDialog(){
+  closeDialog() {
     this.dialogRef.close();
   }
 
+  getLineChartDetails(obj: any) {
+    console.log(obj);
+    let groupId = obj.Obj.standardId <= 2 ? 1 : obj.Obj.standardId <= 5 ? 2 : 3;
+    obj.studentId = obj.Obj.id
+    const objData = {
+      objData: obj,
+      groupId: groupId || 1,
+    }
+    this.webService.selectedLineChartObj.next(JSON.stringify(objData));
+  }
 }
